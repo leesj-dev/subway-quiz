0409fbe2f788e4cdcdfea1b277bf81ec7b8e7eec
leesj-dev <104193215+leesj-dev@users.noreply.github.com>
2023-04-10 23:48:45 +0900

    v3 update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let solved = 0
-let stations = new Array();
+let solved = 0;
+let timeout;
 let timer;
 let min, sec;
 const twoStations = ["동래", "부전", "좌천"];
@@ -18,16 +18,15 @@ document.getElementById("answer").addEventListener("submit", function (event) {
             station = station + "_동해선";
         }
         let svgElement = document.getElementById("map").contentDocument.getElementById(station);
-        // 역명이 존재하고, [호선 옵션이 비활성화되어 있거나 svg에 해당 호선이 존재하는 경우]
-        if (svgElement != null && (line === "disabled" || svgElement.classList.contains(line))) {
+        // 역명이 존재하고, svg에 해당 호선이 존재하는 경우
+        if (svgElement != null && svgElement.classList.contains(line)) {
             let currentState = svgElement.getAttribute("visibility");
             if (currentState === "hidden") {
                 svgElement.setAttribute("visibility", "visible");
-                stations.push(svgElement.id);
                 solved += 1;
                 document.getElementById("solved").innerHTML = solved;
                 showPopup("correct");
-                if (document.getElementById("solved").innerHTML == document.getElementById("total").innerHTML) {
+                if (document.getElementById("solved").innerHTML === document.getElementById("total").innerHTML) {
                     document.getElementById("complete").style.display = "block";
                     switchButtons(false);
                 }
@@ -41,14 +40,26 @@ document.getElementById("answer").addEventListener("submit", function (event) {
     document.getElementById("station").value = "";  // 입력한 역명은 clear (호선은 유지)
 })
 
+// 팝업창 띄우기
 function showPopup(e) {
     const item = document.getElementById(e);
+    // 팝업이 2초 내에 두 번 이상 뜰 때 새로 팝업 뜨게 함
+    const allPopups = document.getElementsByClassName("popup");
+    clearTimeout(timeout);
+    for (let i = 0; i < allPopups.length; i++) {
+        allPopups[i].style.display = "none";
+    }
+    item.classList.remove('popup');  // reset animation
+    void item.offsetWidth;  // trigger reflow
+    item.classList.add('popup');  // start animation
     item.style.display = "inline-block";
-    setTimeout(function () { item.style.display = "none"; }, 2000);
+    timeout = setTimeout(function () { item.style.display = "none"; }, 2000);
 }
 
+// 호선 선택 시 init option 제거
 function optionCheck(e) {
     if (e != "") {
+        document.getElementById("line").style.boxShadow = "inset 1px 1px 2px #BABECC, inset -1px -1px 2px #FFFFFF";
         try {
             document.getElementById("default-option").outerHTML = "";
         } catch (e) {
@@ -57,59 +68,67 @@ function optionCheck(e) {
     }
 }
 
+// 시작 <-> 포기 시 버튼 활성화/비활성화
 function switchButtons(param) {
-    if (param === true) {
+    if (param === true) { // 시작을 눌렀을 때
         document.getElementById("start").disabled = false;
+        document.getElementById("start").classList = ["button-on"];
+        document.getElementById("give-up").disabled = true;
+        document.getElementById("give-up").classList = ["button-off"];
+        document.getElementById("submit").disabled = true;
+        document.getElementById("submit").classList = ["button-off"];
         document.getElementById("line").disabled = true;
         document.getElementById("station").disabled = true;
-        document.getElementById("submit").disabled = true;
-        document.getElementById("start").style.color = "#61677C"
-    } else if (param === false) {
+    } else if (param === false) { // 포기를 눌렀거나 종료될 때
         document.getElementById("start").disabled = true;
+        document.getElementById("start").classList = ["button-off"];
+        document.getElementById("give-up").disabled = false;
+        document.getElementById("give-up").classList = ["button-on"];
+        document.getElementById("submit").disabled = false;
+        document.getElementById("submit").classList = ["button-on"];
         document.getElementById("line").disabled = false;
         document.getElementById("station").disabled = false;
-        document.getElementById("submit").disabled = false;
-        document.getElementById("start").style.color = "#B1B4C0"
     }
 }
 
+// 타이머 시작
 function startTimer() {
+    document.getElementById("solved").innerHTML = 0;  // solved 변수 초기화
+    let previousCorrect = document.getElementById("map").contentDocument.querySelectorAll('[visibility="visible"]');
+    for (let i = 0; i < previousCorrect.length; i++) {
+        previousCorrect[i].setAttribute("fill", "black");
+        previousCorrect[i].setAttribute("font-weight", "regular");
+        previousCorrect[i].setAttribute("visibility", "hidden");
+    }
     min = 10;
     sec = 0;
-    timer = setInterval(countTimer, 1000);
+    timer = setInterval(function() {
+        if (min === 0 && sec === 0) {
+            clearTimer(timer, "시간 초과");
+        } else {
+            if (sec === 0) {
+                min--;
+                sec = 59;
+            } else {
+                sec--;
+            }
+            let minT = min < 10 ? "0" + min : min;
+            let secT = sec < 10 ? "0" + sec : sec;
+            document.getElementById("display").innerText = minT + " : " + secT;
+        }
+    }, 1000);
     switchButtons(false);
 }
 
-function countTimer() {
-    if (min === 0 && sec === 0) {
-        clearTimer(timer, "TIME OUT");
-    } else {
-        if (sec === 0) {
-            min--;
-            sec = 59;
-        } else {
-            sec--;
-        }
-        let minT = min < 10 ? "0" + min : min;
-        let secT = sec < 10 ? "0" + sec : sec;
-        document.getElementById("display").innerText = minT + " : " + secT;
-    }
-}
-
-
-function resetTimer() {
-    clearTimer(timer, "10 : 00");
-    switchButtons(true);
-    document.getElementById("solved").innerHTML = 0;
-}
-
+// 타이머 종료
 function clearTimer(t, text) {
     clearInterval(t);
     document.getElementById("display").innerText = text;
-
-    // 기존에 표시되었던 역을 다시 숨김
-    for (let i = 0; i < stations.length; i++) {
-        let svgElement = document.getElementById("map").contentDocument.getElementById(stations[i]);
-        svgElement.setAttribute("visibility", "hidden");
+    let incorrect = document.getElementById("map").contentDocument.querySelectorAll('[visibility="hidden"]');
+    for (let i = 0; i < incorrect.length; i++) {
+        incorrect[i].setAttribute("fill", "red");
+        incorrect[i].setAttribute("font-weight", "bold");
+        incorrect[i].setAttribute("visibility", "visible");
     }
+    switchButtons(true);
 }
